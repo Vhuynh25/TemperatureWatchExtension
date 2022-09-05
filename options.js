cff04c053ddf50dffab2af1ad1cdd2a88f3c9ccd
Vien Huynh <vienth123@gmail.com>
2022-09-05 13:14:31 -0400

    added options page javascript

---
+++ options.js
@@ -0,0 +1,40 @@
+function saveOptions()
+{
+    var max_check = document.getElementById("max_check").value
+    var min_check = document.getElementById("min_check").value
+
+    var max_threshold = document.getElementById("max_num").value
+    var min_threshold = document.getElementById("min_num").value
+
+    var unit = document.getElementById("unit").value
+
+    chrome.storage.sync.set({
+        maxCheck: max_check,
+        minCheck: min_check,
+        maxThres: max_threshold,
+        minThres: min_threshold,
+        unit: unit,
+    })
+}
+
+function restoreOptions()
+{
+    chrome.storage.sync.get({
+        maxCheck: false,
+        minCheck: false,
+        maxThres: 100,
+        minThres: -50,
+        unit: "fahrenheit"
+    }, function (items) {
+        document.getElementById("max_check").value = items.maxCheck
+        document.getElementById("min_check").value = items.minCheck
+
+        document.getElementById("max_num").value = items.maxThres
+        document.getElementById("min_num").value = items.minThres
+
+        document.getElementById("unit").value = items.unit
+    })
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions)
+document.getElementById("save").addEventListener('click', saveOptions)
