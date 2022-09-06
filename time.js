617544ad2849d8f0ee29ce11197ef202d3a9a43f
Vien Huynh <vienth123@gmail.com>
2022-09-05 21:49:37 -0400

    removed checkForNewDay

--- time.js
+++ time.js
@@ -1,26 +1,12 @@
 const DAY = 24 * 60 * 60 * 1000
 
-export function checkForNewDay() 
-{
-    if (localStorage.savedTimestamp) 
-    {
-        let timestamp = parseInt(localStorage.savedTimestamp)
-
-        if (Date.now() >= timestamp)
-            return true
-        else 
-            return false
-    }
-    return true
-}
-
-export function calculateNewDay()
+export function calculateTomorrowsDate()
 {
     let nextDay = new Date(Date.now() + DAY)
     next.setHours(7)
     next.setMinutes(0)
 
-    localStorage.savedTimestamp = next.getTime()
+    return next.getTime()
 }
 
 export function getTodaysDate()
