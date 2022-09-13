381d9f4a38f87fbfac1c9c6ea852d1dcf834e27a
Vien Huynh <vienth123@gmail.com>
2022-09-12 20:44:42 -0400

    changed from "daily" to "hourly"

--- popup.js
+++ popup.js
@@ -1,7 +1,5 @@
 import * as time_help from "./time.js"
 
-
-
 function runApp(){
     getLocation()
 
@@ -52,7 +50,7 @@ function fetchFromLocation(latitude, longitude)
             const end = time_help.getTomorrowsDate()
             const start = time_help.getTodaysDate()
 
-            const response = await fetch(`${BASEURL}latitude=${latitude}&longitude=${longitude}&temperature_unit=${items.unit}&daily=temperature_2m_max,temperature_2m_min&timezone=auto&start_date=${start}&end_date=${end}`)
+            const response = await fetch(`${BASEURL}latitude=${latitude}&longitude=${longitude}&temperature_unit=${items.unit}&hourly=temperature_2m_max,temperature_2m_min&timezone=auto&start_date=${start}&end_date=${end}`)
             await response.json().then(async function (data){
                 const temperatures = parseWeatherJson(data)
                 console.log("checking thresholds\n")
@@ -122,7 +120,7 @@ async function checkMin(temp)
 
 function parseWeatherJson(json)
 {
-    const dailyData = json["daily"]
+    const dailyData = json["hourly"]
     const maxTemp = Math.max(...dailyData["temperature_2m_max"])
     const minTemp = Math.min(...dailyData["temperature_2m_min"])
 
